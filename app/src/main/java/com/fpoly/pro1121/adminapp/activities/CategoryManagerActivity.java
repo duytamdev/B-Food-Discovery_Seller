e00a3ad7e405caf7a410306c351519a7c88868ff
Nguyen Duy Tam <duytam.dev@gmail.com>
2021-11-04 06:35:33 +0700

    add layout category manager and product manager

--- app/src/main/java/com/fpoly/pro1121/adminapp/activities/CategoryManagerActivity.java
+++ app/src/main/java/com/fpoly/pro1121/adminapp/activities/CategoryManagerActivity.java
@@ -1,16 +1,26 @@
 package com.fpoly.pro1121.adminapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 import com.fpoly.pro1121.adminapp.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class CategoryManagerActivity extends AppCompatActivity {
 
+    RecyclerView rvCategory;
+    FloatingActionButton fabAddCategory;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category_manager);
+        initUI();
+    }
+
+    private void initUI() {
+        rvCategory = findViewById(R.id.rv_category);
+        fabAddCategory = findViewById(R.id.fab_add_category);
     }
 }
--- app/src/main/java/com/fpoly/pro1121/adminapp/activities/ProductManagerActivity.java
+++ app/src/main/java/com/fpoly/pro1121/adminapp/activities/ProductManagerActivity.java
@@ -1,16 +1,26 @@
 package com.fpoly.pro1121.adminapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 import com.fpoly.pro1121.adminapp.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class ProductManagerActivity extends AppCompatActivity {
 
+    RecyclerView rvProduct;
+    FloatingActionButton  fabAddProduct;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_manager);
+        initUI();
+    }
+
+    private void initUI() {
+        rvProduct = findViewById(R.id.rv_product);
+        fabAddProduct = findViewById(R.id.fab_add_product);
     }
 }
